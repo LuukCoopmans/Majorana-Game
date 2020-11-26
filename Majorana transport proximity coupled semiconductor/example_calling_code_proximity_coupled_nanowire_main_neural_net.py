24cbf69e210cf0cedef687f4aaa7260fc70315f3
LuukCoopmans <coopmanl@tcd.ie>
2020-11-26 10:59:23 +0000

    Proximity coupled

---
+++ Majorana transport proximity coupled semiconductor/example_calling_code_proximity_coupled_nanowire_main_neural_net.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+    Main code for training a n.n to find the optimal wall profile using 
+    differential programming. 
+"""
+
+import jax
+from jax.config import config
+config.update("jax_enable_x64", True)
+import jax.numpy as np
+from jax import grad, jacrev
+from jax.experimental import stax, optimizers
+from jax.experimental.stax import Conv, Dense, MaxPool, Relu, Flatten, LogSoftmax, Sigmoid
+from jax import random
+from majorana_transport_proximity_coupled_nanowire import *
+#from NNfunctions import *
+import matplotlib.pyplot as plt
+import time
+
+start_time = time.time()
+N = 50             # Lattice sites
+mu_phys = 0.8         # Chemical potential
+m = 0.5           # Electron mass
+t = 1/(2*m)
+b = 1.0          # Zeeman field in x-direction
+alpha = 1.0*2       # Rashba Spin-orbit interaction strength
+delta = 0.5       # Superconducting gap
+
+T_total = 4.0
+dt = 0.1
+tlist = np.linspace(0,T_total,int(T_total/dt))
+    
+#Define the initial external potential profile parameters 
+Vmax = 30.1*np.ones(len(tlist))
+sigma = 1*np.ones(len(tlist))
+xL_0 = 5.0
+xR = float(N-5)
+
+
+#Define the parameters for motion of the left wall
+xL_f = [6.32]
+
+neurons1 = 100
+neurons2 = 100
+neurons3 = 100
+#layer2_sizes=[5,7,10,15,20]
+LR=2e-3 #learning rate for the optimizer
+episodes = 1
+
+final_fidelity_neurons2 = [] 
+for i in range(len(xL_f)):
+    
+
+    net_init, net_apply = stax.serial(
+            Dense(neurons1), Relu,
+            Dense(neurons2), Relu,
+            Dense(neurons3), Relu,
+            Dense(1), Sigmoid
+            )
+
+    # Initialize parameters, not committing to a batch shape
+
+    rng = random.PRNGKey(1)
+    in_shape = (-1, 1,)
+    out_shape, net_params = net_init(rng, in_shape)
+    net_params = [[a.astype(np.float64) for a in d] for d in net_params] # convert data type
+    inputs = np.array([[k] for k in tlist[1:len(tlist)]/T_total])
+
+    def get_xL(net_params, inputs, xL_0, xL_f):
+        predictions = net_apply(net_params, inputs)
+        ratio = predictions[:, 0]
+        ratio = np.concatenate((np.array([0]),ratio))
+        ratio = np.concatenate((ratio,np.array([1])))
+        xL = xL_0 + (xL_f - xL_0) * ratio
+        #plt.plot(predictions)
+        return xL
+ 
+    # define loss
+    def loss(net_params, inputs, xL_0, xL_f, N,Vmax,sigma,xR,mu_phys,Delta,t,b,alpha,dt):
+        predictions2 = net_apply(net_params, inputs)
+        ratio2 = predictions2[:, 0]
+        ratio2 = np.concatenate((np.array([0]),ratio2))
+        ratio2 = np.concatenate((ratio2,np.array([1])))
+        xL = xL_0 + (xL_f - xL_0) * ratio2
+        return -Transport_Majorana(N,Vmax,sigma,xL,xR,mu_phys,Delta,t,b,alpha,dt);
+
+#learning step
+    def step(i, opt_state, inputs, xL_0, xL_f, N,Vmax,sigma,xR,mu_phys,Delta,t,b,alpha,dt):
+        p = get_params(opt_state)
+        g = grad(loss)(p, inputs, xL_0, xL_f, N,Vmax,sigma,xR,mu_phys,Delta,t,b,alpha,dt)
+        return opt_update(i, g, opt_state)
+
+    # Apply network to get xL
+    xL = get_xL(net_params, inputs, xL_0, xL_f[i])
+    xL2 = np.linspace(xL_0, xL_f[i], len(tlist), endpoint=True)
+
+
+    Overlap1 = Transport_Majorana(N,Vmax,sigma,xL,xR,mu_phys,delta,t,b,alpha,dt);
+    Overlap2 = Transport_Majorana(N,Vmax,sigma,xL2,xR,mu_phys,delta,t,b,alpha,dt);
+
+    # define optimizer
+    ##TODO: large scale test with more steps
+    opt_init, opt_update, get_params = optimizers.adam(step_size=LR)
+    opt_state = opt_init(net_params)
+
+    filename_parameters = 'N100_mu_min055_Delta_05_t_1_T_12_dt_01_Vmax_301_sigma_1_xL0_5_xlF_932_layer_1_neurons_{}__layer_2_neurons_{}_activation1_Relu_activation2_neurons_layer3_{}_sigmoid_alpha1e-3_episodes_200'.format(neurons1,neurons2,neurons3)
+    learning_curve = []
+    for j in range(episodes):
+        then = time.time()
+        opt_state = step(i, opt_state, inputs,xL_0, xL_f[i], N,Vmax,sigma,xR,mu_phys,delta,t,b,alpha,dt)
+        net_params_tmp = get_params(opt_state)
+        xL_tmp = get_xL(net_params_tmp, inputs, xL_0, xL_f[i])
+        plt.figure(4)
+        plt.plot(xL_tmp)
+        Overlap_tmp = Transport_Majorana(N,Vmax,sigma,xL_tmp,xR,mu_phys,delta,t,b,alpha,dt)
+        learning_curve.append(Overlap_tmp)
+        print(neurons1,1-Overlap_tmp,'The learning episode {} took: '.format(j), time.time()-then, 'seconds')
+
+    net_params = get_params(opt_state)
+    xL3 = get_xL(net_params, inputs, xL_0, xL_f[i])
+    Overlap3 = Transport_Majorana(N,Vmax,sigma,xL3,xR,mu_phys,delta,t,b,alpha,dt);
+    print('compare different overlaps')
+    print(neurons1, Overlap1)
+    print(neurons1, Overlap2)
+    print(neurons1, Overlap3)
+
+    #Postprocessing saving and plotting
+    final_fidelity_neurons2.append(Overlap3)
+    #Qloss_inbetween_final_wall = Transport_Majorana_all_fidelities(N,Vmax,sigma,xL3,xR,mu_phys,delta,t,b,alpha,dt)
+
+    #save the data
+    #np.save('Data2/Qloss(t)_{}'.format(filename_parameters),Qloss_inbetween_final_wall)
+    np.save('Data2/Final_wall(t)_{}'.format(filename_parameters),xL3)
+    np.save('Data2/Learning_curve_{}'.format(filename_parameters),learning_curve)
+    np.save('Data2/NN_weight_params_{}'.format(filename_parameters),net_params)
+
+    tlist = np.linspace(0,T_total,len(xL3))
+    plt.figure(4)
+    plt.xlabel('time t/dt')
+    plt.ylabel('Left wall position x_L(t)')
+    plt.savefig('figures2/Path_search_space_N110_mu_03_Delta_1_t_1_T_5_dt_01_Vmax_301_sigma_1_xL0_5_xlF_10_layer_1_neurons_{}_layer_2_neurons_{}_activation1_Relu_activation2_sigmoid_neurons3_{}'.format(neurons1, neurons2,neurons3))
+
+    #plot the final wall profile
+    plt.figure(1)
+    plt.clf()
+    plt.xlabel('time t')
+    plt.ylabel('Left wall position x_L(t)')
+    plt.plot(tlist,xL3)
+    plt.savefig('figures2/Final_wall(t)_{}'.format(filename_parameters))
+
+
+    #fidelity as a function of time up to T 
+    
+    #plt.figure(2)
+    #plt.clf()
+    #plt.plot(tlist, Qloss_inbetween_final_wall)
+    #plt.xlabel('time t')
+    #plt.ylabel('Fidelity F(t)')
+    #plt.savefig('figures2/Qloss(t)_{}'.format(filename_parameters))
+
+    #plot the learning curve
+    plt.figure(3)
+    plt.clf()
+    plt.plot(learning_curve)
+    plt.ylabel('Final Fidelity at T_total')
+    plt.xlabel('Learning episode number')
+    plt.savefig('figures2/Learning_curve_{}'.format(filename_parameters))
+
+    print('Total learning took:', time.time()-start_time,'for neurons',neurons2)
+    #plt.show()
+
+#np.save('final_fidelity_neurons3layers',final_fidelity_neurons2)
+    
+assert False, 'stop'
+
+
+
---
+++ Majorana transport proximity coupled semiconductor/majorana_transport_proximity_coupled_nanowire.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 14 13:50:53 2020
+S-wave spin orbit coupled verison majorana transport code
+@author: lcoopmans
+"""
+import jax
+from jax.config import config
+config.update("jax_enable_x64", True)
+import jax.numpy as np
+
+
+def Potentialprofile(N,Vmax,sigma,xL,xR):
+    """
+        Returns the Potentialprofile for particular sigma, xL, xR and Vmax.
+    """
+    V = Vmax/(1+np.exp((np.arange(N)-xL)/sigma)) + Vmax/(1+np.exp(-(np.arange(N)-xR)/sigma))
+    return V
+
+def Swave_spin_orbit_Hamiltonian(N,mu_phys,delta,t,b,alpha):
+    """
+        Returns the Swave spin-orbit coupled Hamiltonian for open boundary conditions.
+    """
+    mu = (-2*t + mu_phys)*np.ones(N)
+    T = t*np.ones(N-1)
+    BN = b*np.ones(N)
+    Alpha = alpha*np.ones(N-1)
+    Delta = delta*np.ones(N)
+
+    A1 = np.diag(-T,-1) + np.diag(-mu,0)+ np.diag(-T,1)
+    A2 = np.diag(Alpha/2,-1) + np.diag(BN,0)+ np.diag(-Alpha/2,1)
+    A = np.concatenate((np.concatenate((A1,A2),axis=1),np.concatenate((A2.conj().T,A1),axis=1)))
+
+    B1 = np.zeros([N,N])
+    B2 = np.diag(Delta,0)
+    B = np.concatenate((np.concatenate((B1,B2),axis=1),np.concatenate((-B2,B1),axis=1)))
+
+    H = np.concatenate((np.concatenate((A,B),axis=1),np.concatenate((B.conj().T,-A.conj().T),axis=1)))
+    return H
+
+def Diagonalization(H_BdG,N):
+    """
+        Diagonalizes the BdG Hamiltonian and returns the W matrix and E.
+    """
+    #N = np.int(np.shape(H_BdG)[0]/2)
+    
+    D, v = np.linalg.eigh(H_BdG)
+
+    U1 = v[0:N,N:2*N];
+    V1 = v[N:2*N,N:2*N];
+
+    W = np.concatenate((np.concatenate((U1,V1.conj()),axis=1),np.concatenate((V1,U1.conj()),axis=1)))
+    E = np.diag(np.dot(np.transpose(W).conj(),np.dot(H_BdG,W)));
+    return np.real(E), W
+
+
+##TODO: check conj() and inv equivalence
+def Evolution_operator(E,W,del_t):
+    """
+         Returns the unitary evolution operator
+    """
+    #U_time = np.dot(np.dot(W,np.diag(np.exp(-1j*del_t*E))),np.linalg.inv(W))
+    U_time = np.dot(W,np.diag(np.exp(-1j*del_t*E)))
+    Wi = W.T.conj()
+    U_time = np.dot(U_time,Wi)
+    return U_time
+
+def Overlap(W_i,W_ev,N):
+    """
+        Computes the overlap between the evolved state and the initial state
+        with the Onishi Formula. 
+    """
+    #N = np.int(np.shape(W_i)[0]/2)
+    
+    U_i = W_i[0:N,0:N];
+    V_i  = W_i[N:2*N,0:N];
+
+    #switch zero modes
+    P1 = np.diag(np.concatenate((np.array([0]),np.ones((N-1)))));
+    P = np.concatenate((np.concatenate((P1,(np.identity(N)-P1)),axis=1),np.concatenate(((np.identity(N)-P1),P1),axis=1)));
+    w_i = np.dot(W_i,P);
+    U_i2 = w_i[0:N,0:N];
+    V_i2  = w_i[N:2*N,0:N];
+
+    U_ev = W_ev[0:N,0:N];
+    V_ev  = W_ev[N:2*N,0:N];
+
+    Overlap1 = np.abs(np.linalg.det((np.dot(U_i.conj().T,U_ev)+np.dot(V_i.conj().T,V_ev))));
+    Overlap2 = np.abs(np.linalg.det((np.dot(U_i2.conj().T,U_ev)+np.dot(V_i2.conj().T,V_ev))));
+
+    return np.max([Overlap1,Overlap2])
+
+def Transport_Majorana(N,Vmax,sigma,xL,xR,mu_phys,Delta,t,b,alpha,dt):
+    Qloss = [];
+
+    #Construct and Diagonalize the initial BdG Hamiltonian
+    V = Potentialprofile(N,Vmax[0],sigma[0],xL[0],xR);
+
+    H_Vcom = Swave_spin_orbit_Hamiltonian(N,mu_phys,Delta,t,b,alpha)
+    H_BdG = H_Vcom + np.diag(np.concatenate((V,V,-V,-V)))
+    H_BdG = 1/2*(H_BdG+H_BdG.conj().T)   #make sure H is hermitian
+
+    E, W_evolved = Diagonalization(H_BdG,N+N)
+
+    for j in range(len(xL)):
+        #Construct and Diagonalize the new BdG Hamiltonian
+        V = Potentialprofile(N,Vmax[j],sigma[j],xL[j],xR);
+        H_BdG = H_Vcom + np.diag(np.concatenate((V,V,-V,-V)))
+        H_BdG = 1/2*(H_BdG+H_BdG.conj().T)
+        E_t, W_t = Diagonalization(H_BdG,N+N)
+
+        U = Evolution_operator(E_t,W_t,dt);
+
+        W_evolved = np.dot(U,W_evolved)
+        #Qloss.append(Overlap(W_t,W_evolved))
+
+    Overlap1 = Overlap(W_t,W_evolved,N+N)
+
+    return Overlap1
+
+def Transport_Majorana_all_fidelities(N,Vmax,sigma,xL,xR,mu_phys,Delta,t,b,alpha,dt):
+    Qloss = [];
+
+    #Construct and Diagonalize the initial BdG Hamiltonian
+    V = Potentialprofile(N,Vmax[0],sigma[0],xL[0],xR);
+
+    H_Vcom = Swave_spin_orbit_Hamiltonian(N,mu_phys,Delta,t,b,alpha)
+    H_BdG = H_Vcom + np.diag(np.concatenate((V,V,-V,-V)))
+    H_BdG = 1/2*(H_BdG+H_BdG.conj().T)   #make sure H is hermitian
+
+    E, W_evolved = Diagonalization(H_BdG,N+N)
+
+    for j in range(len(xL)):
+        #Construct and Diagonalize the new BdG Hamiltonian
+        V = Potentialprofile(N,Vmax[j],sigma[j],xL[j],xR);
+        H_BdG = H_Vcom + np.diag(np.concatenate((V,V,-V,-V)))
+        H_BdG = 1/2*(H_BdG+H_BdG.conj().T)
+        E_t, W_t = Diagonalization(H_BdG,N+N)
+
+        U = Evolution_operator(E_t,W_t,dt);
+
+        W_evolved = np.dot(U,W_evolved)
+        Qloss.append(Overlap(W_t,W_evolved))
+
+    Overlap1 = Overlap(W_t,W_evolved,N+N)
+
+    return Qloss
+
